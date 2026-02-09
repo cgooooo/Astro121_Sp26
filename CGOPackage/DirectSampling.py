2793d3b0683031d75277150344890d5cc4ce045f
cgooooo <157634662+cgooooo@users.noreply.github.com>
2026-02-09 10:07:49 -0800

    Update DirectSampling.py

--- CGOPackage/DirectSampling.py
+++ CGOPackage/DirectSampling.py
@@ -19,11 +19,6 @@ filename = f"testing_{sample_rate}Hz"
 sdr = ugradio.sdr.SDR(direct=True, sample_rate=sample_rate)
 
 
-#if use_custom_fir:
-    #fir_coeffs = np.array()
-    #sdr.set_fir_coeffs(fir_coeffs)
-
-
 
 # Actual data capture
 data = sdr.capture_data(nblocks=2)[1]
